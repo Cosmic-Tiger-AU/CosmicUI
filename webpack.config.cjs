124c68d7ac2d97222bf64d6956479c6d30b7d589
Toby Boulton <toby@cosmictiger.com.au>
2023-11-11 19:27:48 +1100

    moar

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "module",
+    clean: true,
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
