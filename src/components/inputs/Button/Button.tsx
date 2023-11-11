3bfaa7376d2247fe59005dd47c8c40a51cb84f5c
Toby Boulton <toby@cosmictiger.com.au>
2023-11-11 21:17:47 +1100

    before themes

--- src/components/inputs/Button/Button.tsx
+++ src/components/inputs/Button/Button.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
+import { ButtonProps } from "./Button.types";
+import { cn } from "../../../utils/cn";
 
-export const Button: React.FC = () => {
-  return <button>Button</button>;
-};
+// Button forward ref
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { children, className, ...rest } = props;
+
+    return (
+      <button {...rest} ref={ref} className={cn(className, "dark:text-white")}>
+        {children}
+      </button>
+    );
+  },
+);
