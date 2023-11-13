b00720b1300df414d7dea4e9944605336133362f
Toby Boulton <toby@cosmictiger.com.au>
2023-11-13 21:55:55 +1100

    yummy rollups?

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import del from "rollup-plugin-delete";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "src/index.ts", // path to your main TS file, change accordingly
+    output: [
+      {
+        format: "cjs",
+        file: "dist/index.js",
+        sourcemap: true,
+      },
+      {
+        format: "esm",
+        file: "dist/index.es.js",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      terser(),
+      postcss({
+        plugins: [], // Add postcss plugins here
+        minimize: true,
+      }),
+      del({ targets: "dist/**/*" }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
+  },
+];
--- src/components/inputs/Button/Button.tsx
+++ src/components/inputs/Button/Button.tsx
@@ -1,17 +1,41 @@
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from "@fortawesome/react-fontawesome";
 import * as React from "react";
-import { ButtonProps } from "./Button.types";
 import { cn } from "../../../utils/cn";
 
-// Button forward ref
+export type ButtonProps = {
+  onClick?: () => void;
+  children: React.ReactNode;
+  icon?: FontAwesomeIconProps["icon"];
+  iconPosition?: "left" | "right";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (props, ref) => {
-    const { children, className, ...rest } = props;
+    const {
+      children,
+      className,
+      icon,
+      iconPosition = "right",
+      ...rest
+    } = props;
 
     return (
       <button {...rest} ref={ref} className={cn(className, "dark:text-white")}>
-        {children}
+        {icon && iconPosition === "left" && (
+          <FontAwesomeIcon icon={icon} className="mr-2" />
+        )}
+        <span className="uppercase">{children}</span>
+        {icon && iconPosition === "right" && (
+          <span className="ml-2">
+            <FontAwesomeIcon icon={icon} />
+          </span>
+        )}
       </button>
     );
   },
 );
+
+Button.displayName = "Button";
