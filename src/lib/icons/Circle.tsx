77f48b0fb491ae563a1329eb26202d0184715ea6
Toby Boulton <toby@cosmictiger.com.au>
2023-12-08 00:14:10 +1100

    fix(icons): fixed icons fill color to current

--- src/lib/icons/Circle.tsx
+++ src/lib/icons/Circle.tsx
@@ -6,6 +6,7 @@ export const Circle = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width={16}
     height={16}
+    fill="currentColor"
     viewBox="0 0 512 512"
     {...props}>
     <path d="M464 256a208 208 0 1 0-416 0 208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0 256 256 0 1 1-512 0z" />
